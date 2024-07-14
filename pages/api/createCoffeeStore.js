ae06f912c29b6589f016a676e1133384df15fef3
Alexandru <alexandru.gurau@live.com>
2024-07-14 08:18:46 +0200

    chore: Update coffee store page to use API endpoint for fetching coffee stores by location

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,19 +1,41 @@
-// pat5GiO6AHaQ8PTjQ.4ab55060668b3ad11a322426bc218fd2d9d58769f3f6691c1df5b5028ec57eb9
-
 const Airtable = require('airtable');
 const base = new Airtable({ apiKey: process.env.AIRTALBE_API_KEY }).base(
   process.env.AIRTABLE_BASE_KEY
 );
 
-const table = base('Coffee Stores');
+const table = base('coffee-stores');
 
 console.log({ table });
 
 const createCoffeeStore = async (req, res) => {
   if (req.method === 'POST') {
-    return res.status(200).json({ message: 'Create Coffee Store' });
-  } else {
-    return res.status(405).json({ message: 'Method Not Allowed' });
+    // find a record
+
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id=0`,
+        })
+        .firstPage();
+
+      console.log({ findCoffeeStoreRecords });
+
+      if (findCoffeeStoreRecords.length > 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+
+        res.json(records);
+      } else {
+        // creating a record
+        res.json({ message: 'Creating a record' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Something went wrong' });
+    }
   }
 };
 
